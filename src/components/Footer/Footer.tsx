2ddc66081a9527377e08c02176399970ce9b024e
WinikeCuamba <WinikeCuamba@github.com>
2024-01-08 22:54:54 +0200

    feat: create component footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="h-16 bg-boost-30 text-boost-60 text-xs flex items-center justify-center">
+     <strong>Boost Store</strong> - transformando a sua marca
+    </footer>
+  )
+}
